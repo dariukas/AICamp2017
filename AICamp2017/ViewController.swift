9fea2d5924296bbc8f4a78f257dabad71f825c03
dariukas <dariui.miliauskui@gmail.com>
2017-04-09 01:32:43 +0300

    Inferencing Added Commit

--- AICamp2017/ViewController.swift
+++ AICamp2017/ViewController.swift
@@ -20,12 +20,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    //https://github.com/Swift-AI/Swift-AI/blob/master/Documentation/NeuralNet.md#multi-layer-feed-forward-neural-network
     func run() {
         do {
             let structure = try NeuralNet.Structure(inputs: 2, hidden: 2, outputs: 1)
             let config = try NeuralNet.Configuration(activation: .sigmoid, learningRate: 0.5, momentum: 0.3)
             let nn = try NeuralNet(structure: structure, config: config)
-            training(neuralNet: nn)
+            //training(neuralNet: nn)
+            inferencing(neuralNet: nn)
         }
         catch let specialError as NSError {
             print(specialError.description)
@@ -33,25 +36,40 @@ class ViewController: UIViewController {
     }
     
     func training(neuralNet: NeuralNet) {
-
+        
         let myTrainingData: [[Float]] = [[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]]
-        let myTrainingLabels: [[Float]] = [[1.0, 1.0]]
+        let myTrainingLabels: [[Float]] = [[1.0], [0.0], [0.0], [0.0]]
         let myValidationData = myTrainingData as [[Float]]
         let myValidationLabels = myTrainingLabels as [[Float]]
         
         do {
-            //let structure = try NeuralNet.Structure(inputs: 2, hidden: 2, outputs: 1)
             let dataset = try NeuralNet.Dataset(trainInputs: myTrainingData, trainLabels: myTrainingLabels, validationInputs: myValidationData, validationLabels: myValidationLabels, structure: neuralNet.structure)
             let weigths = try neuralNet.train(dataset, cost: .crossEntropy, errorThreshold: 0.001)
             print("Result \(weigths)")
-            print(neuralNet.allWeights())
-            
+            //print(neuralNet.allWeights())
         }
         catch let error as NSError  {
-            print(error)
+            print(error.description)
         }
-        
     }
     
+    //https://blogs.nvidia.com/blog/2016/08/22/difference-deep-learning-training-inference-ai/
+    func inferencing(neuralNet: NeuralNet) {
+        let weights: [Float] =  [1.27217102, -2.62812805, -2.74673152, -2.59496784, 5.33755922, 5.21428537, 2.4808259, 4.77823496, -9.98672009]
+        do{
+            try neuralNet.setWeights(weights)
+        }
+        catch let error as NSError  {
+            print(error.description)
+        }
+        
+        let input: [Float] = [0.0, 0.0]
+        do{
+            let output = try neuralNet.infer(input)
+            print(output)        }
+        catch let error as NSError  {
+            print(error.description)
+        }
+    }
 }
 
