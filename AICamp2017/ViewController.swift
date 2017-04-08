55057c3697f55f8e43a2cff417cd36748ee24afa
dariukas <dariui.miliauskui@gmail.com>
2017-04-09 00:20:01 +0300

    Swift-AI Added Commit

--- AICamp2017/ViewController.swift
+++ AICamp2017/ViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func run() {
+        do {
+            let structure = try NeuralNet.Structure(inputs: 784, hidden: 420, outputs: 10)
+            let config = try NeuralNet.Configuration(activation: .sigmoid, learningRate: 0.5, momentum: 0.3)
+            let nn = try NeuralNet(structure: structure, config: config)
+        }
+        catch let specialError as NSError {
+            print(specialError.description)
+        }
+    }
+    
+    func training() {
+        let dataset = try NeuralNet.Dataset(trainInputs: myTrainingData,
+                                            trainLabels: myTrainingLabels,
+                                            validationInputs: myValidationData,
+                                            validationLabels: myValidationLabels,
+                                            structure: structure)
+        
+    }
+    
 }
 
